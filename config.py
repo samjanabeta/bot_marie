ba0d39011acffaaa347a28fe801808be817673ca
mariabereznaa21 <mariabereznaa21@gmail.com>
2020-08-27 18:46:10 +0600

    правки

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # bot token from @BotFather
-API_TOKEN = '1271565350:AAEM5k7CygUVJIhjVg-M9zi5vZ0EQmcNSr4'
-DB_HOST = 'mysql:3306'
+API_TOKEN = '1365780866:AAGIytYU7tIm4Aej-AzdH0ajZFt_i21kJLw'
+DB_HOST = 'localhost'
 DB_USER = 'root'
 DB_PASSWORD = '123456'
 DB_NAME = 'marie_bot'
